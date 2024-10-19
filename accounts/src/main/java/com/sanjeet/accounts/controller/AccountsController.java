1949e793bdf8dfcc25687300ab251d10415db627
Sanjeet Kumar Nayak <40313847+nayaksanjeet@users.noreply.github.com>
2024-10-19 14:21:21 +0530

    added various property reading mechanism of Spring framework in accounts microservices

--- accounts/src/main/java/com/sanjeet/accounts/controller/AccountsController.java
+++ accounts/src/main/java/com/sanjeet/accounts/controller/AccountsController.java
@@ -1,6 +1,7 @@
 package com.sanjeet.accounts.controller;
 
 import com.sanjeet.accounts.constants.AccountsConstants;
+import com.sanjeet.accounts.dto.AccountsContactDto;
 import com.sanjeet.accounts.dto.CustomerDto;
 import com.sanjeet.accounts.dto.ErrorResponseDto;
 import com.sanjeet.accounts.dto.RsponseDto;
@@ -13,16 +14,18 @@ import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.Pattern;
-import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.core.env.Environment;
 
 @RestController
 @RequestMapping(path = "api/v1/accounts", produces = {MediaType.APPLICATION_JSON_VALUE})
-@AllArgsConstructor
+
 @Validated
 @Tag(
         name = "CRUD Rest API for Accounts in My Bank",
@@ -32,6 +35,19 @@ public class AccountsController {
 
     private IAccountsService accountsService;
 
+    @Value("${build.version}")
+    private String buildVersion;
+
+    @Autowired
+    private Environment environment;
+
+    @Autowired
+    private AccountsContactDto accountsContactDto;
+
+    public AccountsController(IAccountsService accountsService) {
+        this.accountsService = accountsService;
+
+    }
 
     @Operation(
             summary = "Open a new Account associated with a customer",
@@ -133,5 +149,49 @@ public class AccountsController {
         else
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).
                     body(new RsponseDto(AccountsConstants.STATUS_417, AccountsConstants.MESSAGE_417_DELETE));
+    }
+
+    @Operation(summary = "Get build version", description = "Get build version that is deployed into accounts  microservice")
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "200", description = "HTTP Status OK"),
+            @ApiResponse(
+                    responseCode = "500", description = "HTTP Status INTERNAL_SERVER_ERROR",
+                    content = @Content(
+                            schema = @Schema(implementation = ErrorResponseDto.class)))
+    })
+    @GetMapping("/build-info")
+    public ResponseEntity<String> getBuildVersion(){
+        return ResponseEntity.ok().body(buildVersion);
+    }
+
+    @Operation(summary = "Get Java version", description = "Get Java version that is installed in the environment of accounts  microservice")
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "200", description = "HTTP Status OK"),
+            @ApiResponse(
+                    responseCode = "500", description = "HTTP Status INTERNAL_SERVER_ERROR",
+                    content = @Content(
+                            schema = @Schema(implementation = ErrorResponseDto.class))
+            )
+    })
+    @GetMapping("/java-version")
+    public ResponseEntity<String> getJavaVersion(){
+        return ResponseEntity.ok().body(environment.getProperty("JAVA_HOME"));
+    }
+
+    @Operation(summary = "Get contact details", description = "Get contact details of accounts microservice in case of any issues")
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "200", description = "HTTP Status OK"),
+            @ApiResponse(
+                    responseCode = "500", description = "HTTP Status INTERNAL_SERVER_ERROR",
+                    content = @Content(
+
+            ))
+    })
+    @GetMapping("/contact-info")
+    public ResponseEntity<AccountsContactDto> getContactInfo(){
+        return ResponseEntity.ok().body(accountsContactDto);
     }
 }
